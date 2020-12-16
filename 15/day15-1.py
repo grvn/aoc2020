0994cd6924ad87373e4ab2f9d9ef74546bcf0855
riched <rickard.hedlund@lm.se>
2020-12-16 13:00:08 +0100

    dag 15

---
+++ 15/day15-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from sys import argv
+from collections import defaultdict
+
+def main():
+  input = ([int(x) for x in open(argv[1]).read().split(',')])
+  previous_seen = defaultdict(list,{key:[index] for index,key in enumerate(input,1)})
+  n = input[-1]
+  for i in range(len(input)+1, 2021):
+    if len(previous_seen[n]) == 1:
+      n = 0
+    else:
+      n = previous_seen[n][-1] - previous_seen[n][-2]
+    previous_seen[n].append(i)
+  print(n)
+
+if __name__ == '__main__':
+  main()
