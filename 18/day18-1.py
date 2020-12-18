5058b1d1442ad0d0a1e0b9615b1d1b8d2ab251dc
riched <rickard.hedlund@lm.se>
2020-12-18 17:59:03 +0100

    dag 18

---
+++ 18/day18-1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from sys import argv
+
+def calculate(input):
+  if input.find('(') == -1:
+    input = [int(x) if x.isdigit() else x for x in input.split()]
+    sum = input[0]
+    for i in range(1,len(input),2):
+      if input[i] == '+':
+        sum += input[i+1]
+      elif input[i] == '*':
+        sum *= input[i+1]
+    return sum
+  else:
+    extra = 0
+    first = input.index('(')
+    for i,char in enumerate(input[first+1:],first):
+      if char == '(': # nested
+        extra += 1
+      elif char == ')':
+        if extra == 0:
+          break
+        else:
+          extra -= 1
+    return calculate(input[:first] + str(calculate(input[first+1:i+1])) + input[i+2:])
+
+def main():
+  with open(argv[1]) as f:
+    input = [line.strip() for line in f]
+  print(sum(calculate(line) for line in input))
+  
+
+if __name__ == '__main__':
+  main()
